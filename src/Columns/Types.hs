c4a154d7fe517a870aa5317657f8bdf3c4758426
Aleksey Pirogov <astynax@users.noreply.github.com>
2017-12-27 22:36:32 +0300

    refactor types

--- src/Columns/Types.hs
+++ src/Columns/Types.hs
@@ -8,32 +8,36 @@ module Columns.Types where
 import Data.Function
 import Data.String
 
+data Game = Game
+  { currentPlayer :: Player
+  , board :: Board
+  } deriving (Show)
+
 data Player
   = First
   | Second
   deriving (Eq, Show)
 
-data D4 = D1 | D2 | D3 | D4 deriving (Show, Eq, Ord)
+type Board = Array2d Cell
+
+type Cell = Maybe (Player, N4 Piece)
 
 data X
 data Y
+data Piece
 
-newtype Coord t = Coord D4 deriving (Show, Eq, Ord)
+data N4 tag
+  = D1
+  | D2
+  | D3
+  | D4
+  deriving (Show, Eq, Ord)
 
-data Array4 t a =
+data Array4 tag a =
   Array4 a a a a
   deriving (Show, Functor, Traversable, Foldable)
 
 type Array2d a = Array4 Y (Array4 X a)
-
-type Cell = Maybe (Player, D4)
-
-type Board = Array2d Cell
-
-data Game = Game
-  { currentPlayer :: Player
-  , board :: Array2d Cell
-  } deriving (Show)
 
 newGame :: Game
 newGame = Game First b
@@ -43,31 +47,31 @@ newGame = Game First b
     f = Just (First, D1)
     s = Just (Second, D1)
     b = empty
-      & update2d (Coord D1) (Coord D1) (const f)
-      & update2d (Coord D1) (Coord D2) (const f)
-      & update2d (Coord D1) (Coord D3) (const f)
-      & update2d (Coord D1) (Coord D4) (const f)
-      & update2d (Coord D4) (Coord D1) (const s)
-      & update2d (Coord D4) (Coord D2) (const s)
-      & update2d (Coord D4) (Coord D3) (const s)
-      & update2d (Coord D4) (Coord D4) (const s)
+      & update2d D1 D1 (const f)
+      & update2d D1 D2 (const f)
+      & update2d D1 D3 (const f)
+      & update2d D1 D4 (const f)
+      & update2d D4 D1 (const s)
+      & update2d D4 D2 (const s)
+      & update2d D4 D3 (const s)
+      & update2d D4 D4 (const s)
 
-getA4 :: Coord t -> Array4 t a -> a
-getA4 (Coord D1) (Array4 x _ _ _) = x
-getA4 (Coord D2) (Array4 _ x _ _) = x
-getA4 (Coord D3) (Array4 _ _ x _) = x
-getA4 (Coord D4) (Array4 _ _ _ x) = x
+getA4 :: N4 t -> Array4 t a -> a
+getA4 D1 (Array4 x _ _ _) = x
+getA4 D2 (Array4 _ x _ _) = x
+getA4 D3 (Array4 _ _ x _) = x
+getA4 D4 (Array4 _ _ _ x) = x
 
-putA4 :: Coord t -> a -> Array4 t a -> Array4 t a
-putA4 (Coord D1) x (Array4 _ b c d) = Array4 x b c d
-putA4 (Coord D2) x (Array4 a _ c d) = Array4 a x c d
-putA4 (Coord D3) x (Array4 a b _ d) = Array4 a b x d
-putA4 (Coord D4) x (Array4 a b c _) = Array4 a b c x
+putA4 :: N4 t -> a -> Array4 t a -> Array4 t a
+putA4 D1 x (Array4 _ b c d) = Array4 x b c d
+putA4 D2 x (Array4 a _ c d) = Array4 a x c d
+putA4 D3 x (Array4 a b _ d) = Array4 a b x d
+putA4 D4 x (Array4 a b c _) = Array4 a b c x
 
-updateA4 :: Coord t -> (a -> a) -> Array4 t a -> Array4 t a
+updateA4 :: N4 t -> (a -> a) -> Array4 t a -> Array4 t a
 updateA4 i f a = putA4 i (f $ getA4 i a) a
 
-update2d :: Coord X -> Coord Y -> (a -> a) -> Array2d a -> Array2d a
+update2d :: N4 X -> N4 Y -> (a -> a) -> Array2d a -> Array2d a
 update2d x y = updateA4 y . updateA4 x
 
 instance IsString Board where
